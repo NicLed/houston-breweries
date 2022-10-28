d924c2d6fe7a8591146e9e6da3d62542bcb15472
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-10-28 18:34:28 -0500

    Update get request URL for getAllHoustonBreweries controller to include all breweries

--- server/controllers/getAllHoustonBreweries.ts
+++ server/controllers/getAllHoustonBreweries.ts
@@ -1,5 +1,5 @@
 const axios = require('axios');
 
 export const getAllHoustonBreweries = () => {
-  return axios.get('https://api.openbrewerydb.org/breweries?by_city=Houston');
+  return axios.get('https://api.openbrewerydb.org/breweries?by_city=Houston&per_page=50');
 };
