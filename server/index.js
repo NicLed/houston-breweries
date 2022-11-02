b4a3fd8a648f394e42835e1d22f4883f805ec743
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-11-01 22:24:07 -0500

    Add path on server index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 const PORT = 4444;
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => {
   `Listening on port ${PORT}`;
