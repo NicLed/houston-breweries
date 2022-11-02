8d7a16621fe095e663a67de975fb9654ca10584b
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-11-01 22:32:45 -0500

    Update quotes on server index for consistency

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 4444;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
   `Listening on port ${PORT}`;
