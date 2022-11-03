8af5c397328e803205b7815c0509f406bdf23f90
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-11-02 19:09:43 -0500

    Change resolve to join

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, '/dist/'),
+    path: path.join(__dirname, '/dist/'),
     filename: 'bundle.js'
   },
   module: {
